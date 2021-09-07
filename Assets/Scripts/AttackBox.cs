d438abab9a54c6c8dd16a08e1a7909eeb40a20a6
jeremy-grondin <jerem97480@gmail.com>
2021-09-07 12:21:50 +0200

    basic combo system for player
    3 combo are available, need to polish

--- Assets/Scripts/AttackBox.cs
+++ Assets/Scripts/AttackBox.cs
@@ -6,7 +6,23 @@ public class AttackBox : MonoBehaviour
     [SerializeField] float attackWindow = 0.5f;
     float attackTimer = 0.0f;
 
+    [SerializeField] float ComboUptime = 2.0f;
+    float comboTimer = 0.0f;
+
+    BoxCollider2D boxCollider;
+    SpriteRenderer sp;
+
     bool hasHit = true;
+
+    bool hasAttack = false;
+
+    int comboCounter = 0;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        sp = GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
@@ -18,17 +34,33 @@ public class AttackBox : MonoBehaviour
                 FinishAttack();
             }
         }
+
+        //Combo
+        if(hasAttack)
+        {
+            comboTimer += Time.deltaTime;
+            if(comboTimer >= ComboUptime || comboCounter == 3)
+            {
+                comboCounter = 0;
+                hasAttack = false;
+                comboTimer = 0.0f;
+            }
+        }
     }
 
     public void Attack()
     {
-        gameObject.SetActive(true);
+        hasAttack = true;
+        boxCollider.enabled = true;
+        sp.enabled = true;
         attackTimer = 0.0f;
     }
 
     public void FinishAttack()
     {
-        gameObject.SetActive(false);
+        boxCollider.enabled = false;
+        sp.enabled = false;
+        hasAttack = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -37,6 +69,8 @@ public class AttackBox : MonoBehaviour
         {
             Enemy en = collision.gameObject.GetComponent<Enemy>();
             en.takeDamage(1);
+            comboCounter++;
+            Debug.Log("Combo : " + comboCounter);
         };
     }
 }
